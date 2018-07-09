fddc74301b76f4b0ecd1996138f8fbadd1d37dc2
rizariza69 <riza.riza69@gmail.com>
2018-07-09 14:24:33 +0700

    git riza

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+Latar Belakang
+Pendidikan terakhir S1 - Fakultas Pertanian UNPAD Jurusan Sosial Ekonomi Pertanian Pekerjaan terakhir dibidang EvenOrganizer 
+
+Motivasi
+Ingin merubah karir menjadi seorang progrmmmer
+
+Ekspektasi
+Mendapat pekerjaan sebagai programmer 3-4 tahun, lalu Bisa membuat startup sendiri
+
+*/
